f083128db396ac4c663f639d329ada7962e3bf2b
Shawn <shawn.busuttil@betssongroup.com>
2017-04-21 12:08:47 +0200

    fixed prod url

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,8 +4,7 @@ const productionUrl = "mongodb://shawn92:admin@ds147799.mlab.com:47799/todos-db"
 const devUrl = "mongodb://localhost:27017/TodoApp";
 
 mongoose.Promise = global.Promise;
-console.log(process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI || devUrl);
+mongoose.connect(process.env.MONGODB_URI || productionUrl || devUrl);
 
 mongoose.exports = {
 	mongoose: mongoose
